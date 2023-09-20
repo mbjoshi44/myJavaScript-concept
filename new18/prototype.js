7ae126c9188a0993426390b71ac1a617c2b2d092
mbjoshi44 <119665891+mbjoshi44@users.noreply.github.com>
2023-09-20 10:23:14 +0530

    prototype

---
+++ new18/prototype.js
@@ -0,0 +1,23 @@
+//JavaScript create an bydefault property called [[prototype]] to the object.
+// javaScript contains many helper properties and methods which can use to complete task
+let array = [10,20,30,40,50];
+console.log(array.length);
+
+// Prototype Inheritance
+// By using prototype we can inherit one object propeties into other
+//eg.
+
+let human = {
+  canfly:false,
+  cantalk:true,
+  canwalk:true
+}
+
+let person ={
+    name:"moti",
+    age:25
+}
+
+person.__proto__ = human
+
+person.canfly //also work
